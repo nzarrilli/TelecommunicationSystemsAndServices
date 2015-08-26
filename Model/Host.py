c15953fbbf66a8a85c54608f78b98a8296958785
Nicola Zarrilli <nicola.zarrilli@gmail.com>
2015-08-26 23:56:50 +0200

    Modified host object

--- Model/Host.py
+++ Model/Host.py
@@ -2,9 +2,6 @@ __author__ = 'nzarrilli'
 
 
 class Host(object):
-    mac_address = ""
-    port = 0
-    dpid = 0
 
     # The class "constructor" - It's actually an initializer
     def __init__(self, mac_address, port, dpid):
